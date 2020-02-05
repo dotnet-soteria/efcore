f753318a4d95cf5ce675e7c88163ba39a661e8db
Isaac Hili <isaac.ih.hili@gmail.com>
2020-02-05 18:16:41 +0100

    Create interceptor for Insecure Direct Object Reference

---
+++ src/Soteria.EntityFrameworkCore.Abstractions/IIdorInterceptor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Soteria.EntityFrameworkCore.Abstractions
+{
+	/// <summary>
+	/// Prevents Insecure Direct Object Reference (IDOR) from occurring whilst retaining the inherent indexing benefits from sequential identifiers.
+	/// </summary>
+	/// <remarks>
+	/// IDOR occurs when a application exposes a reference to an internal implementation object. Using this way, it reveals the real identifier and format/pattern used of the element in the storage backend side.
+	/// <a href="https://owasp.org/www-project-cheat-sheets/cheatsheets/Insecure_Direct_Object_Reference_Prevention_Cheat_Sheet.html">Read more</a>
+	/// </remarks>
+	public interface IIdorInterceptor
+	{
+		/// <summary>
+		/// Unique IDOR-safe identifier.
+		/// </summary>
+		Guid Id { get; set; }
+	}
+}
